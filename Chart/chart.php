29d0aa310fb95789c49b490e0333606627ab08ed
CernavcaValeria <61517848+CernavcaValeria@users.noreply.github.com>
2021-04-21 12:04:32 +0300

    Update chart.php

--- Chart/chart.php
+++ Chart/chart.php
@@ -194,7 +194,10 @@
     <div class="loader-section section-left"></div>
     <div class="loader-section section-right"></div>
   </div>  
-  <script src="js/jquery-3.5.1.min.js"></script>
+  <script
+  src="https://code.jquery.com/jquery-3.6.0.js"
+  integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
+  crossorigin="anonymous"></script>
   <script src="js/bootstrap.min.js"></script>
   <script src="js/slick.js"></script>
   <script src="js/templatemo-script.js"></script>
